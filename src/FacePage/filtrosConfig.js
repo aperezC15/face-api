c6a1efe8bc33109fb9afcd32223c44c58a5958a4
Albi Pérez <albiperez1997@gmail.com>
2020-05-07 08:29:33 -0600

    Crear un módulo para definir los filtros.

---
+++ src/FacePage/filtrosConfig.js
@@ -0,0 +1,43 @@
+const filtros = [
+	{
+		name: 'Cocina1',
+		path: './Imagenes/Cocina/Cocina1.svg',
+		positionFace: 'eyeLeft',
+		dim: {
+			width: '100px',
+			height: '100px'
+		},
+		move: {
+			toRight: '0px',
+			toBottom: '0px'
+		}
+	},
+	{
+		name: 'Cocina2',
+		path: './Imagenes/Cocina/Cocina2.svg',
+		positionFace: 'eyeLeft',
+		dim: {
+			width: '100px',
+			height: '100px'
+		},
+		move: {
+			toRight: '0px',
+			toBottom: '0px'
+		}
+	},
+	{
+		name: 'Cocina3',
+		path: './Imagenes/Cocina/Cocina3.svg',
+		positionFace: 'eyeLeft',
+		dim: {
+			width: '100px',
+			height: '100px'
+		},
+		move: {
+			toRight: '0px',
+			toBottom: '0px'
+		}
+	}
+];
+
+export default filtros;
