68fdc3f87ca6ebe01efb072e060f3f27e2888a83
Albi Pérez <albiperez1997@gmail.com>
2020-05-07 09:59:06 -0600

    Arreglar bugs de dimensiones.

--- src/FacePage/filtrosConfig.js
+++ src/FacePage/filtrosConfig.js
@@ -2,196 +2,196 @@ const filtros = [
 	{
 		name: 'Cocina1',
 		path: './Imagenes/Cocina/Cocina1.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina2',
 		path: './Imagenes/Cocina/Cocina2.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina3',
 		path: './Imagenes/Cocina/Cocina3.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina4',
 		path: './Imagenes/Cocina/Cocina4.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina5',
 		path: './Imagenes/Cocina/Cocina5.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina6',
 		path: './Imagenes/Cocina/Cocina6.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina7',
 		path: './Imagenes/Cocina/Cocina7.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina8',
 		path: './Imagenes/Cocina/Cocina8.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina9',
 		path: './Imagenes/Cocina/Cocina9.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina10',
 		path: './Imagenes/Cocina/Cocina10.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina11',
 		path: './Imagenes/Cocina/Cocina11.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina12',
 		path: './Imagenes/Cocina/Cocina12.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina13',
 		path: './Imagenes/Cocina/Cocina13.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina14',
 		path: './Imagenes/Cocina/Cocina14.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	},
 	{
 		name: 'Cocina15',
 		path: './Imagenes/Cocina/Cocina15.svg',
-		positionFace: 'eyeLeft',
+		positionFace: 'getLeftEye',
 		dim: {
-			width: '100px',
-			height: '100px'
+			width: '100',
+			height: '100'
 		},
 		move: {
-			toRight: '0px',
-			toBottom: '0px'
+			toRight: '0',
+			toBottom: '0'
 		}
 	}
 ];
